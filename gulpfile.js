b2d2ae5e0fc2fb42f8f0a7d7bf35abf1ad4f1b68
Dawid Galeziewski <dawidgalsino@vp.pl>
2019-06-22 19:58:48 +0200

    set up browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 // Import modules
 const gulp = require('gulp'); 
 const sass = require('gulp-sass');
-const browserSync  = require('browser-sync');
+const browserSync  = require('browser-sync').create();
+const autoprefixer = require('autoprefixer');
 
 // gulp.task('sass', () => { return gulp.src('app/scss/**/*.scss')
 // .pipe(sass())
@@ -21,6 +22,33 @@ function style(){
     .pipe(sass())
     // 3. Where do I save the complied CSS?
     .pipe(gulp.dest('app/css'))
+    // 4. Stream changes to all browsers
+    .pipe(browserSync.stream())
 }
 
+// watcher
+function watch(){
+    // specify server to run
+    browserSync.init({
+        server: {
+            // server directory
+            baseDir:'./app/'
+        }
+    });
+
+    // Watch for any changes on scss files
+        // run style task to complie any changes
+    gulp.watch('app/scss/**/*.scss', style);
+
+    // Watch for any changes in index files, reload browser on those changes
+    gulp.watch('./*.html').on('change', browserSync.reload);
+
+    // Watch for javaScript changes, reload on change
+    gulp.watch('app/js/**/*.js').on('change', browserSync.reload);
+
+}
+
+
+// export tasks
 exports.style = style
+exports.watch = watch
